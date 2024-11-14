a0720b4b0a7ebdd9a7afb01e7244245304fe8467
Luiz Fernando <lfernando488@gmail.com>
2024-11-13 23:57:05 -0300

    Get game by id

--- src/main/java/com/devsuperior/dslist/Controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/Controllers/GameController.java
@@ -1,10 +1,13 @@
 package com.devsuperior.dslist.Controllers;
 
+import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.DTO.GameMinDTO;
 import com.devsuperior.dslist.Entities.Game;
 import com.devsuperior.dslist.Services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,17 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = null;
+        try {
+            result = gameService.findById(id);
+        } catch (Exception e) {
+            return new GameDTO();
+        }
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
--- src/main/java/com/devsuperior/dslist/Services/GameService.java
+++ src/main/java/com/devsuperior/dslist/Services/GameService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dslist.Services;
+import com.devsuperior.dslist.DTO.GameDTO;
 import com.devsuperior.dslist.Entities.Game;
 import com.devsuperior.dslist.Repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +14,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true) //Indica que nao será feita uma operação de escrita, nao irá prender a base
+    public GameDTO findById(Long id) throws Exception {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<Game> findAll(){
         List<Game>result = gameRepository.findAll();
         return  result;
